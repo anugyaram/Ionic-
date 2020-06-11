e90c52d8965161cccbcec83e59bf79b5042fd934
anugya ram <anugyaram08@gmail.com>
2020-06-11 20:12:47 +0530

    Assignment 3

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
-import { baseURL } from '../../shared/baseurl';
+import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 
 /*
@@ -18,14 +18,22 @@ export class FavoriteProvider {
   favorites: Array<any>;
 
 
-  constructor(public http: Http, private dishservice: DishProvider) {
+  constructor(public http: Http, private dishservice: DishProvider, private storage: Storage) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
+    storage.get('favorites').then(favorites=>{
+      if(favorites){
+        console.log(favorites);
+        this.favorites = favorites;
+       }
+    })
   }
 
   addFavorite(id: number): boolean{
-    if(!this.isFavorite(id))
+    if(!this.isFavorite(id)){
       this.favorites.push(id);
+      this.storage.set('favorites', this.favorites);
+    }
     console.log('favorites', this.favorites);
     return true;
   }
@@ -43,6 +51,7 @@ export class FavoriteProvider {
     let index = this.favorites.indexOf(id);
     if(index >= 0){
       this.favorites.splice(index,1);
+      this.storage.set('favorites', this.favorites);
       return this.getFavorites();
     }
     else{
